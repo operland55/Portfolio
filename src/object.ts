71301d24da1e6d400fe9f74f551576779459239a
jungpa <operland55@naver.com>
2022-12-03 23:18:47 +0900

    ✨ Projects Object Create

---
+++ src/object.ts
@@ -0,0 +1,58 @@
+import MovieSite from "./assets/movieScreen.jpg";
+import TomorrowSite from "./assets/Tomorrow-Home-Desk.jpg";
+
+export const Projects = [
+	{
+		name: "SnowRainCloud",
+		img: `${MovieSite}`,
+		skill: ["sass", "react", "reactQuery", "recoil"],
+		homepage: "https://relaxed-crumble-8ec459.netlify.app/",
+		period: "2022-10-01 ~ 2022-11-01",
+		introduce:
+			"처음으로 혼자 모든걸 만든 사이트입니다. 아무래도 넷플릭스 사이트를 많은분들이 포트폴리어로 올리다보니 한번 저만의 영화 소개 사이트를 만들고싶어서 사이트를 구경하면서 마음에 든 디자인을보고 기억해 두었다가 만들었습니다. 힘들었 던 점은 아무래도 레이아웃 짜는것과 라이브러리로 비록 구현하였지만 박스안에 여러가지 영화들을 자동으로 슬라이드 시키는게 힘들었습니다",
+	},
+
+	{
+		name: "Tomorrow-HomePage",
+		img: `${TomorrowSite}`,
+		skill: ["html", "sass", "JavaScript"],
+		homepage: "https://app.netlify.com/sites/bucolic-puppy-4b4fce/overview",
+		period: "2022-07-01 ~ 2022-08-01",
+		introduce:
+			"이 사이트같은경우는 처음부터 끝까지 클론코딩으로 만든 사이트입니다.  가장 재밌고 부족한 점들을 보완을 많이 했습니다. 우선 레이아웃 짜는것과 그리고 퍼스트반응형 Sass입니다 저는 Sass를 배우기전에 style-component로만 화면을 구현하였는데 sass를 배우고나서는 sass만 사용했던것같습니다 ",
+	},
+	{
+		name: "NetFlex",
+		img: `${MovieSite}`,
+		skill: ["react", "reactQuery", "typeScript"],
+		homepage: "https://app.netlify.com/sites/beautiful-custard-737306/overview",
+		period: "2022-05-01 ~ 2022-05-16",
+		introduce:
+			"노마드 코더의 강의를듣고 기본틀만 잡아주고 나머지는 혼자서 만든 홈페이지입니다. 아쉬웠던 점은 반응형 페이지를 제대로 못만들고 영하와 Tv Detail 중심만 만들었습니다.	재미있었던 부분은 원하는 데이터들의 api를 하나하나 들고와 정보를 얻어서 꾸밀수 있었 던 점이 재미있었습니다",
+	},
+
+	{
+		name: "zzzzzzzzzzzzzzz",
+		img: `${MovieSite}`,
+		skill: ["react", "reactQuery", "typeScript"],
+		homepage: "https://app.netlify.com/sites/beautiful-custard-737306/overview",
+		period: "2022-05-01 ~ 2022-05-16",
+		introduce: "노마드 ",
+	},
+	{
+		name: "zzzzzzzzzzzzzzz2",
+		img: `${MovieSite}`,
+		skill: ["react", "reactQuery", "typeScript"],
+		homepage: "https://app.netlify.com/sites/beautiful-custard-737306/overview",
+		period: "2022-05-01 ~ 2022-05-16",
+		introduce: "노마드 ",
+	},
+	{
+		name: "zzzzzzzzzzzzzzz5",
+		img: `${MovieSite}`,
+		skill: ["react", "reactQuery", "typeScript"],
+		homepage: "https://app.netlify.com/sites/beautiful-custard-737306/overview",
+		period: "2022-05-01 ~ 2022-05-16",
+		introduce: "노마드 ",
+	},
+];
