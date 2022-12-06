21f489a1d998fc355653d54d5a93fa757a6f5666
jungpa <operland55@naver.com>
2022-12-06 20:59:13 +0900

    📝 Create Project Component

--- src/Components/Project.tsx
+++ src/Components/Project.tsx
@@ -7,6 +7,12 @@ import Header from "./Header";
 import ProjectDetail from "./ProjectDetail";
 import { motion, AnimatePresence, useViewportScroll } from "framer-motion";
 import { MdArrowBackIosNew, MdArrowForwardIos } from "react-icons/md";
+
+const Content = styled.div`
+	position: relative;
+	margin: 0 auto;
+	max-width: 1140px;
+`;
 const Title = styled.h1`
 	text-align: center;
 	color: ${(props) => props.theme.textColor};
@@ -14,28 +20,50 @@ const Title = styled.h1`
 	margin-top: 80px;
 	margin-bottom: 20px;
 	@media ${(props) => props.theme.tablet} {
-		margin-top: 0;
+		margin-top: 114px;
 	}
 `;
-const Content = styled.div``;
+const NextBtn = styled(MoveButton)`
+	right: 0;
+	@media ${(props) => props.theme.desktop} {
+		right: 4vw;
+		svg {
+			font-size: 34px;
+		}
+	}
+`;
+const PrevBtn = styled(MoveButton)`
+	left: 0;
+	@media ${(props) => props.theme.desktop} {
+		left: 4vw;
+		svg {
+			font-size: 34px;
+		}
+	}
+`;
 const Items = styled(motion.ul)`
-	position: relative;
-	display: flex;
+	padding-top: 10vw;
+	@media ${(props) => props.theme.tablet} {
+		padding-top: 0;
+		height: calc(100vh - 360px);
+	}
 `;
 const Item = styled(motion.li)`
+	color: ${(props) => props.theme.textColor};
 	padding: 0 20px;
 	display: flex;
 	width: 100%;
-	position: absolute;
 	flex-direction: column;
 	align-items: center;
-	border-radius: 40px;
 
 	@media ${(props) => props.theme.tablet} {
+		flex-direction: row;
+		justify-content: center;
+		min-height: 500px;
+		gap: 6vw;
 	}
 `;
 const ImgBox = styled(motion.div)`
-	width: 300px;
 	height: 220px;
 	img {
 		display: block;
@@ -43,31 +71,45 @@ const ImgBox = styled(motion.div)`
 		width: 100%;
 		height: 100%;
 	}
-`;
-const NextBtn = styled(MoveButton)`
-	right: 5px;
-`;
-const PrevBtn = styled(MoveButton)`
-	left: 5px;
+	@media ${(props) => props.theme.tablet} {
+		width: 350px;
+	}
 `;
 
+const SiteInfo = styled(FlexBox)`
+	flex-direction: column;
+	font-size: 3.7vw;
+	@media ${(props) => props.theme.tablet} {
+		align-items: flex-start;
+		max-width: 400px;
+		font-size: 16px;
+	}
+`;
 const SiteName = styled.p`
 	margin-top: 5.3vw;
 `;
 const SimPleInfo = styled.p`
 	margin-top: 5.3vw;
 	line-height: 1.5;
+	@media ${(props) => props.theme.tablet} {
+		margin-top: 20px;
+	}
 `;
 
 const DetailBtn = styled.button`
+	color: ${(props) => props.theme.textColor};
+	background-color: ${(props) => props.theme.btnBgColor};
 	align-self: flex-end;
 	padding: 5px 10px;
-	background-color: ${(props) => props.theme.btnBgColor};
 	border-radius: 10px;
 	margin-top: 2.6vw;
+	cursor: pointer;
 	outline: none;
 	border: none;
-	flex-grow: 1;
+
+	@media ${(props) => props.theme.tablet} {
+		padding: 10px 20px;
+	}
 `;
 const Overlay = styled(motion.div)`
 	position: absolute;
@@ -154,32 +196,34 @@ function Project() {
 
 	return (
 		<>
-			<Container>
-				<Content>
-					<Title>ProJect</Title>
-					<NextBtn>
-						<MdArrowForwardIos onClick={nextPlease} />
-					</NextBtn>
-					<PrevBtn>
-						<MdArrowBackIosNew onClick={prevPlease} />
-					</PrevBtn>
-					<Items>
-						<AnimatePresence>
-							{Projects.map((item: any, key: number) =>
-								visible === key + 1 ? (
-									<>
-										<Item
-											custom={back}
-											key={key}
-											variants={box}
-											initial="invisible"
-											animate="visible"
-											exit="exit"
-											layoutId={item.name}
-										>
-											<ImgBox>
-												<img src={item.img} alt="Homepage Img" />
-											</ImgBox>
+			<Content>
+				<Title>ProJect</Title>
+				<NextBtn>
+					<MdArrowForwardIos onClick={nextPlease} />
+				</NextBtn>
+				<PrevBtn>
+					<MdArrowBackIosNew onClick={prevPlease} />
+				</PrevBtn>
+
+				<Items>
+					<AnimatePresence>
+						{Projects.map((item: any, key: number) =>
+							visible === key + 1 ? (
+								<>
+									<Item
+										custom={back}
+										key={key}
+										variants={box}
+										initial="invisible"
+										animate="visible"
+										exit="exit"
+										layoutId={item.name}
+									>
+										<ImgBox>
+											<img src={item.img} alt="Homepage Img" />
+										</ImgBox>
+
+										<SiteInfo>
 											<SiteName>{item.name}</SiteName>
 											<SimPleInfo>{item.introduce}</SimPleInfo>
 
@@ -190,31 +234,31 @@ function Project() {
 											>
 												상세정보
 											</DetailBtn>
-										</Item>
-									</>
-								) : null
-							)}
-						</AnimatePresence>
-					</Items>
-				</Content>
-				<AnimatePresence>
-					{proJectMatch ? (
-						<>
-							<Overlay
-								animate={{ opacity: 1 }}
-								exit={{ opacity: 0 }}
-								onClick={() => navigate(-1)}
-							/>
-							<BigProject
-								style={{ top: scrollY.get() + 20 }}
-								layoutId={proJectMatch.params.id}
-							>
-								<ProjectDetail DetailId={proJectMatch.params.id} />
-							</BigProject>
-						</>
-					) : null}
-				</AnimatePresence>{" "}
-			</Container>
+										</SiteInfo>
+									</Item>
+								</>
+							) : null
+						)}
+					</AnimatePresence>
+				</Items>
+			</Content>
+			<AnimatePresence>
+				{proJectMatch ? (
+					<>
+						<Overlay
+							animate={{ opacity: 1 }}
+							exit={{ opacity: 0 }}
+							onClick={() => navigate(-1)}
+						/>
+						<BigProject
+							style={{ top: scrollY.get() + 20 }}
+							layoutId={proJectMatch.params.id}
+						>
+							<ProjectDetail DetailId={proJectMatch.params.id} />
+						</BigProject>
+					</>
+				) : null}
+			</AnimatePresence>{" "}
 		</>
 	);
 }
